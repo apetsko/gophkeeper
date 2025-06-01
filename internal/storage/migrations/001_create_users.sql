b78d7a2d7524cacbc7915a8def8573fd6dcbb57a
apetsko <apetskom@gmail.com>
2025-06-01 19:27:57 +0500

    migrations: add otp fileds, remove unused tables

--- internal/storage/migrations/001_create_users.sql
+++ internal/storage/migrations/001_create_users.sql
@@ -4,8 +4,10 @@ CREATE TABLE IF NOT EXISTS users
     id            SERIAL PRIMARY KEY,
     username      VARCHAR(255) UNIQUE NOT NULL,
     password_hash TEXT                NOT NULL,
-    created_at    TIMESTAMPTZ      DEFAULT now(),
-    updated_at    TIMESTAMPTZ      DEFAULT now()
+    totp_secret   TEXT,
+    totp_enabled  BOOLEAN     DEFAULT FALSE,
+    created_at    TIMESTAMPTZ DEFAULT now(),
+    updated_at    TIMESTAMPTZ DEFAULT now()
 );
 -- +goose Down
 DROP TABLE IF EXISTS users;
