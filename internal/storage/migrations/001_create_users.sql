f784ecb924724516ba88be8b13cf8c2cdf9dc55b
apetsko <apetskom@gmail.com>
2025-06-14 00:26:30 +0500

    users table

--- internal/storage/migrations/001_create_users.sql
+++ internal/storage/migrations/001_create_users.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS users
 (
-    id            SERIAL PRIMARY KEY,
+    id            INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     username      VARCHAR(255) UNIQUE NOT NULL,
-    password_hash TEXT                NOT NULL,
+    password_hash VARCHAR(60)         NOT NULL,
     totp_secret   TEXT,
     totp_enabled  BOOLEAN     DEFAULT FALSE,
     created_at    TIMESTAMPTZ DEFAULT now(),
